c219dc4a2b2001e80cb119dd1c3efb2b0653dea5
luisgc93 <luisgc93@gmail.com>
2021-08-28 23:35:02 +0200

    alembic config needs to import models

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,7 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 from app.core.models.database import Base
+from app.core.models import models # noqa
 
 config = context.config
 
