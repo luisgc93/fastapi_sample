a1fa456fe474cc9cc37b527c4ed016b41844dcee
luisgc93 <luisgc93@gmail.com>
2021-09-05 00:40:15 +0200

    add debug

--- app/core/models/database.py
+++ app/core/models/database.py
@@ -4,13 +4,17 @@ import os
 from sqlalchemy import create_engine
 
 # https://stackoverflow.com/questions/15175339/sqlalchemy-what-is-declarative-base/15176114
+from sqlalchemy.engine import make_url
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+# should the url contain the driver name? i.e psycopg2
 
-print("DB URL: " + SQLALCHEMY_DATABASE_URL)
+
+url = make_url(SQLALCHEMY_DATABASE_URL)
+print("MAKE URL RESULT: " + url)
 # https://docs.sqlalchemy.org/en/14/tutorial/engine.html#tutorial-engine
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
