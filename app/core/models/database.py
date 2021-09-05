20af0ff3f73dfa11f63b7f13aa67578e86684c01
luisgc93 <luisgc93@gmail.com>
2021-09-05 17:51:54 +0200

    remove zero div and add db string

--- app/core/models/database.py
+++ app/core/models/database.py
@@ -9,11 +9,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL").replace("://", "ql://", 1)
-err = 1/0
 
 # https://docs.sqlalchemy.org/en/14/tutorial/engine.html#tutorial-engine
 engine = create_engine(
-    DATABASE_URL,
+    "postgresql://qnqdmvyzeatxjz:ed1dbe69e06a9177eec9283aab9832426e9f9fd1a9a31730f5a782f03a90b05f@ec2-34-251-245-108.eu-west-1.compute.amazonaws.com:5432/d1jnt3lodq1srt",
 )
 
 # https://docs.sqlalchemy.org/en/14/orm/session_basics.html
