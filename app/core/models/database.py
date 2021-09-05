c8010ab0e069b2b05ef265bc24137cbe3d16c6e5
luisgc93 <luisgc93@gmail.com>
2021-09-05 16:20:52 +0200

    try to run without migration command

--- app/core/models/database.py
+++ app/core/models/database.py
@@ -4,17 +4,16 @@ import os
 from sqlalchemy import create_engine
 
 # https://stackoverflow.com/questions/15175339/sqlalchemy-what-is-declarative-base/15176114
-from sqlalchemy.engine import make_url
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import sessionmaker
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+DATABASE_URL = os.getenv("DATABASE_URL")  # or other relevant config var
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 # https://docs.sqlalchemy.org/en/14/tutorial/engine.html#tutorial-engine
 engine = create_engine(
-    uri,
+    DATABASE_URL,
 )
 
 # https://docs.sqlalchemy.org/en/14/orm/session_basics.html
