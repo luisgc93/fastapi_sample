8e512519db4d947eeeea371a4d58a63c92e3d02c
luisgc93 <luisgc93@gmail.com>
2021-09-05 18:31:40 +0200

    I think the problem might be here

--- alembic/env.py
+++ alembic/env.py
@@ -17,7 +17,7 @@ config = context.config
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
-config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
+config.set_main_option('sqlalchemy.url', os.getenv('SQLALCHEMY_DATABASE_URL'))
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
