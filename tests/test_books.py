bdede5ddf1d299ac21fe7a6390c7713dc7484edd
luisgc93 <luisgc93@gmail.com>
2021-10-02 23:49:15 +0200

    add 422 test and db query assertion

--- tests/test_books.py
+++ tests/test_books.py
@@ -1,7 +1,9 @@
+from app.core.models import Book
+
 
 class TestBooks:
     def test_returns_200_and_creates_book_when_payload_is_valid(
-            self, client
+            self, client, session
     ):
 
         payload = {
@@ -14,6 +16,20 @@ class TestBooks:
         )
 
         assert response.status_code == 200
+        assert session.query(Book).count() == 1
 
-    def test_returns_422_when_payload_is_invalid(self):
-        pass
+    def test_returns_422_when_payload_is_invalid(
+            self, client, session
+    ):
+
+        payload = {
+            "key": "value",
+        }
+        response = client.post(
+            "/books/",
+            json=payload
+        )
+
+        assert response.status_code == 422
+        assert session.query(Book).count() == 0
+
