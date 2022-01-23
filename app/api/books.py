775ded6d00733dc2cb887c7bfe291bc86ec584c0
luisgc93 <luisgc93@gmail.com>
2022-01-23 21:58:01 +0100

    add redis pubsub

--- app/api/books.py
+++ app/api/books.py
@@ -1,16 +1,21 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from redis import Redis
 from sqlalchemy.orm import Session
 from app.api.users import get_current_user
 from app.core.crud import crud_books
 from app.core.database import get_db
 from app.core import schemas
 
+from app.redis import redis_connect
+
 router = APIRouter(tags=["books"], dependencies=[Depends(get_current_user)])
 
 
 @router.post("/books/")
-async def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
-    # TODO: Validate if a book already exists under that title
+async def create_book(book: schemas.BookCreate, db: Session = Depends(get_db), redis: Redis = Depends(redis_connect)):
+    message = f"New book added: {book.title}"
+    subscribers = redis.publish(channel="book-published", message=message)
+    logger.info(f"Message {message} published to {subscribers} subscribers")
     return crud_books.create_book(db=db, book=book)
 
 
